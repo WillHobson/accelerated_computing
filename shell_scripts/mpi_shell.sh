13b0e928a967a8dacc2db6b2d91c751ee2670faa
WillHobson <ni20088@bristol.ac.uk>
2023-10-25 11:42:56 +0100

    shell script for mpi runs

---
+++ shell_scripts/mpi_shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=lebwohllasher
+#SBATCH --partition=teach_cpu
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:0:10
+#SBATCH --account=PHYS030544
+#SBATCH --mem-per-cpu=100M
+
+mpirun -np 2 python mpi_ll.py 100 10 0.5 0
