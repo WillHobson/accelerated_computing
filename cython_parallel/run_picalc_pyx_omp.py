848069fe0144377c6769908c8b19996ff49bafcc
WillHobson <ni20088@bristol.ac.uk>
2023-11-05 15:55:07 +0000

    removing excess files

--- cython_parallel/run_picalc_pyx_omp.py
+++
@@ -1,8 +0,0 @@
-import sys
-from picalc_pyx_omp import main
-
-if int(len(sys.argv)) == 3:
-    main(int(sys.argv[1]),int(sys.argv[2]))
-else:
-    print("Usage: {} <ITERATIONS> <THREADS>".format(sys.argv[0]))
-
